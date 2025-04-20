1c719e1b6ba01b8277ae4ab45b61b3b654809686
abhi4gitt <raiabhishek631@gmail.com>
2025-04-20 18:21:59 +0530

    dynamic SwiftUI List using Swift Generics and JSON data

---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,19 @@
+//
+//  AnimalModel.swift
+//  Africa
+//
+//  Created by Abhishek on 20/04/25.
+//
+
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
